dadaa4a8e631fe880e1e6bb8fea7d4ad49224c2f
Vishal Sinha <vsinha@playdom.com>
2013-10-29 17:49:13 +0530

    login changes

--- frontend/hobbyMate.php
+++ frontend/hobbyMate.php
@@ -1,5 +1,6 @@
 <?php
 include_once 'common/header.php';
+session_start();
 ?>
 <!DOCTYPE html>
 
--- frontend/signup.php
+++ frontend/signup.php
@@ -72,63 +72,6 @@ session_start();
     </div>
     <!-- End: MAIN CONTENT -->
  
-<!-- Start: MAIN CONTENT -->
-<div class="content">
-	<div class="container">
-		<div class="page-header">
-			<h1>Signup to HobbyMate</h1>
-		</div>
-		<div class="row">
-			<div class="span6 offset3">
-				<h4 class="widget-header">
-					<i class="icon-gift"></i> Be a part of HobbyMate
-				</h4>
-				<div class="widget-body">
-					<div class="center-align">
-						<form class="form-horizontal form-signin-signup"
-							action="/HobbyMate/backend/login.php" method="post">
-							<input type="hidden" name="action" value="create"> <input
-								type="text" name="username" placeholder="Email"> <input
-								type="password" name="password" placeholder="Password"> <input
-								type="password" name="password_confirmation"
-								placeholder="Password Confirmation">
-							<div>
-								<input type="submit" value="Signup"
-									class="btn btn-primary btn-large">
-							</div>
-						</form>
-						<h4>
-							<i class="icon-question-sign"></i> Already have an account?
-						</h4>
-						<a href="signin.html" class="btn btn-large bottom-space">Signin</a>
-						<h4>
-							<i class="icon-thumbs-up"></i> Sign in with third party account
-						</h4>
-						<ul class="signin-with-list">
-							<li><a class="btn-twitter"> <i class="icon-twitter icon-large"></i>
-									Signin with Twitter
-							</a>
-							</li>
-							<li><a class="btn-facebook"> <i class="icon-facebook icon-large"></i>
-									Signin with Facebook
-							</a>
-							</li>
-							<li><a class="btn-google"> <i class="icon-google-plus icon-large"></i>
-									Signin with Google
-							</a>
-							</li>
-							<li><a class="btn-github"> <i class="icon-github icon-large"></i>
-									Signin with Github
-							</a>
-							</li>
-						</ul>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
-<!-- End: MAIN CONTENT -->
 
 <?php
 include_once 'common/footer.php';
