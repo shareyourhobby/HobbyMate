33973cdb5d43a13189d3471f555cd0955006b2e0
Vishal Sinha <vsinha@playdom.com>
2013-11-01 15:56:59 +0530

    remove hobbymate.php

--- frontend/index.php
+++ frontend/index.php
@@ -12,9 +12,7 @@ session_start();
 			<div class="row-fluid">
 				<div class="span6 marketting-info">
 					<h1>Welcome</h1>
-					<h3>We at HobbyMate aim to connect people of similar skills &
-						provide a platform to people who would like to explore their
-						hidden talents.</h3>
+					<h3>At Hobbymate,we aim to help people in learning & utilizing their activity of interest by connecting them with like minded people who share the same interest or finding the right institute for their learning.</h3>
 					<a href="JoinNow.php" class="btn btn-primary">Join US</a>
 				</div>
 				<div class="span6">
@@ -32,7 +30,8 @@ session_start();
 			<ul class="thumbnails">
 				<li class="span4">
 					<div class="thumbnail">
-						<img src="img/placeholder-360x200.jpg" alt="product name">
+						<img src="img/joinUs.jpg" alt="product name">
+						<br/>
 						<div class="caption">
 							<h3>For Learners & Teachers</h3>
 							<p>
@@ -43,18 +42,18 @@ session_start();
 						</div>
 						<div class="widget-footer">
 							<p>
-								<a href="frontend/JoinNow.php" class="btn btn-primary">JOIN NOW</a>&nbsp;
+								<a href="JoinNow.php" class="btn btn-primary">JOIN NOW</a>&nbsp;
 							</p>
 						</div>
 					</div>
 				</li>
 				<li class="span4">
 					<div class="thumbnail">
-						<img src="img/placeholder-360x200.jpg" alt="product name">
+						<img src="img/partner.jpg" alt="product name">
 						<div class="caption">
 							<h3>For Institutes</h3>
-							<p>If you are a institute specialized in teaching of any kind of
-								skill, then you are at the right place. Partner us and get the
+							<p>If you are a institute specialized in teaching any kind of
+								skill, then HobbyMate is the right place for you. Partner us and get the
 								best students. What more! We offer range of services. Get in
 								touch now!!!</p>
 						</div>
@@ -67,13 +66,15 @@ session_start();
 				</li>
 				<li class="span4">
 					<div class="thumbnail">
-						<img src="img/placeholder-360x200.jpg" alt="product name">
+						<img src="img/forum.jpg" alt="product name">
 						<div class="caption">
-							<h3>Marketplace</h3>
-							<p>Looking for your next Guitar, Cycle but worried of initial
-								expense? Hover to the Marketplace and rent your stuff. Quality
-								Assured!!! You can also place your existing stuff on rent!!!</p>
+							<h3>Discussion Forum</h3>
+							<p>At HobbyMate, we beleive in healthy discussion.
+							   Login to forum & discuss the activity of your interest with others.
+							   Start Now!!!
+							   </p>
 						</div>
+						<br/>
 						<div class="widget-footer">
 							<p>
 								<a href="#" class="btn btn-primary">GO TO MARKETPLACE</a>&nbsp;
